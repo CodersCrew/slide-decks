7a0c0510ea0a6dc9e7f14a654194d079a8f49f40
radosbartosz <radosbartosz@gmail.com>
2019-06-12 12:56:00 +0200

    SocialMedia

---
+++ templates/main/slides/SocialMedia/SocialMedia.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { string, exact, arrayOf } from 'prop-types';
+import Item from './Item';
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%:
+  height: 100%:
+`;
+
+const SocialMedia = ({ items, styles }) => (
+  <Container styles={styles}>
+    <div className="Items">
+      {items.map(item => (
+        <Item key={item.image} {...item} />
+      ))}
+    </div>
+  </Container>
+);
+
+SocialMedia.propTypes = {
+  items: arrayOf(
+    exact({
+      image: string,
+      line: string,
+      title: string,
+      content: string,
+    }),
+  ),
+  styles: arrayOf(string),
+};
+
+export default SocialMedia;
