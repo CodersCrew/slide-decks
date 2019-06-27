570c36b800671622e492ee1788e3d2da9cb7ad5b
radosbartosz <radosbartosz@gmail.com>
2019-06-27 13:25:22 +0200

    Toolbar_2

--- slide-deck/Deck/slides-methods.js
+++ slide-deck/Deck/slides-methods.js
@@ -4,8 +4,11 @@ export function nextSlide() {
   this.setState(state => ({
     slideIndex: state.slideIndex + 1,
     animation: { direction: 'next', type: this.slides.find(({ key }) => key === state.slideIndex).animation.type },
-  }));
-}
+    percentage: this.state.percentage + (1/(this.slidesCount-1))*100,
+  }))}
+  else if(slideIndex === this.slidesCount - 1){
+    this.setState({ percentage: 100 });
+  }
 }
 
 export function prevSlide() {
@@ -17,9 +20,14 @@ export function prevSlide() {
       direction: 'prev',
       type: this.slides.find(({ key }) => key === state.slideIndex - 1).animation.type,
     },
+    percentage: this.state.percentage - (1/(this.slidesCount-1))*100,
   }));
 }
+  else if(slideIndex === 0){
+    this.setState({ percentage: 0 });
+  }
 }
+
 
 export function handleArrowPress({ keyCode }) {
   const { slideIndex } = this.state;
