d4f26171fa791eb19bb3bf16acb038da6d25e064
radosbartosz <radosbartosz@gmail.com>
2019-07-07 15:24:14 +0200

    Adding toggle icon

--- templates/main/components/Panel.jsx
+++ templates/main/components/Panel.jsx
@@ -1,18 +1,24 @@
 import styled from 'styled-components';
+import { Component } from 'react';
 
 const Container = styled.div`
     position: absolute;
     bottom: 0;
-    padding: 0 30px 10px 0;
     width: 100%;
     z-index: 10;
     display: flex;
     justify-content: flex-end;
+    padding: 0 20px 7px 0;
    
     p {
         color: var(--blue);
         font-size: 25px;
         font-weight: var(--bold);
+        display: none;
+
+        &.show {
+            display: block;
+        }
     }
 `;
 
@@ -21,12 +27,25 @@ const Wrapper = styled.div`
     flex-direction: row;
     align-items: center; 
     justify-content: center;
-    width: 400px;
-    background-color: rgb(184, 184, 184, 0.5);
-    border-radius: 10px;
-    padding: 10px 10px 0 10px;
+    background: none;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    margin-right: 2px;
+    max-width: 0px;
+    transition: all 1500ms ease;
+    padding: 8px 5px 0 3px;
     
+    &.show {
+        max-width: 400px;
+        background-color: rgb(184, 184, 184, 0.5);
+
+        button {
+            opacity: 1;
+        }
+    }
+
     button {
+        opacity: 0;
         background: none;
         border: none;
         outline: none;
@@ -43,24 +62,57 @@ const SlideNumber = styled.div`
 
 const Arrows = styled.div`
     display: flex;
-    margin-left: 59px;
+    margin-left: 40px;
+    padding-right: 10px;
 `;
 
-const Panel = ({ actualSlide, slides, prevSlide, nextSlide, newTheme }) => {
-    return (
-    <Container className="container">
-        <Wrapper className="wrapper">
-            <button onClick={newTheme}><img src="/static/icons/light/theme_btn.svg"/></button>
-            <Arrows className="arrows">
-                <button onClick={prevSlide}><img src="/static/icons/light/left_arrow.svg"/></button>
-                <SlideNumber className="slide-number">
-                    <p> {actualSlide + 1}|{slides} </p>
-                </SlideNumber>
-                <button onClick={nextSlide}><img src="/static/icons/light/right_arrow.svg"/></button>
-            </Arrows>
-        </Wrapper>
-    </Container>
-    );
+const ShowIcon = styled.button`
+    background-color: rgb(184, 184, 184, 0.5);
+    padding: 10px 10px 5px 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: center; 
+    justify-content: center;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+`;
+
+
+class Panel extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isMinimized: false,
+        }
+    }
+
+    toggleToolbar = () => {
+        const checkTo = !this.state.isMinimized;
+        this.setState({ isMinimized: checkTo });
+    }
+
+    render(){
+        const { actualSlide, slides, prevSlide, nextSlide, newTheme } = this.props;
+        return (
+        <Container>
+            <Wrapper className={ this.state.isMinimized ? "show" : null }>
+                <button onClick={newTheme}><img src="/static/icons/light/theme_btn.svg"/></button>
+                <Arrows className="arrows">
+                    <button onClick={prevSlide}><img src="/static/icons/light/left_arrow.svg"/></button>
+                    <SlideNumber className="slide-number">
+                        <p className={ this.state.isMinimized ? "show" : null }> {actualSlide + 1}|{slides} </p>
+                    </SlideNumber>
+                    <button onClick={nextSlide}><img src="/static/icons/light/right_arrow.svg"/></button>
+                </Arrows>
+            </Wrapper>
+            <ShowIcon onClick={this.toggleToolbar}><img src="/static/icons/light/plus_icon.svg"/></ShowIcon>
+        </Container>
+        );
+    };
 };
 
 export default Panel;
